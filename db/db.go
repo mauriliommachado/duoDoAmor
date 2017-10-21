587aaac20a00a81dbb6329ca759a5e0add5f26a5
Maurílio <mauriliommachado@gmail.com>
2017-10-20 23:07:02 -0200

    a

--- db/db.go
+++ db/db.go
@@ -11,7 +11,7 @@ var db *sql.DB
 
 func Start() {
 	var err error
-	db, err = sql.Open("postgres", "postgres://postgres:admin@localhost/duo?sslmode=disable")
+	db, err = sql.Open("postgres", "postgres://wrzuvgfdhwhxmd:d073a4d6d274727ada015a2ba9cac3d6f50a08afe9e2271a58409cfafcd16d68@ec2-23-21-158-253.compute-1.amazonaws.com:5432/d1dfnfc8hob5ov")
 	if err != nil {
 		log.Fatal(err)
 	}
