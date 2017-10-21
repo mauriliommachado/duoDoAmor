9496f49f6537dfac110e3c04216201b56e994667
Maurílio <mauriliommachado@gmail.com>
2017-10-21 11:47:20 -0200

    a

--- db/db.go
+++ db/db.go
@@ -5,13 +5,14 @@ import (
 	_ "github.com/lib/pq"
 	"log"
 	"fmt"
+	"os"
 )
 
 var db *sql.DB
 
 func Start() {
 	var err error
-	db, err = sql.Open("postgres", "postgres://wrzuvgfdhwhxmd:d073a4d6d274727ada015a2ba9cac3d6f50a08afe9e2271a58409cfafcd16d68@ec2-23-21-158-253.compute-1.amazonaws.com:5432/d1dfnfc8hob5ov")
+	db, err = sql.Open("postgres", determineDb())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -20,4 +21,13 @@ func Start() {
 
 func GetDB() (c *sql.DB) {
 	return db
+}
+
+
+func determineDb() (string) {
+	port := os.Getenv("DATABASE_URL")
+	if port == "" {
+		return "postgres://wrzuvgfdhwhxmd:d073a4d6d274727ada015a2ba9cac3d6f50a08afe9e2271a58409cfafcd16d68@ec2-23-21-158-253.compute-1.amazonaws.com:5432/d1dfnfc8hob5ov"
+	}
+	return port
 }
