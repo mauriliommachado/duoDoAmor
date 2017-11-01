9b9622275a5a1ea535672cb7fa41e69706590b00
Maurílio <mauriliommachado@gmail.com>
2017-11-01 01:13:18 -0200

    a

--- db/match.go
+++ db/match.go
@@ -62,7 +62,7 @@ func (match *Match) FindById() {
 func (match *Match) FindNew() (Users, error) {
 	s := GetDB()
 	var array Users
-	rows, err := s.Query("SELECT u.id, u.\"summonerId\", u.name, u.email, r.\"queueType\", r.tier, r.rank, r.\"leaguePoints\", r.wins, r.losses FROM duo.\"user\" u join duo.rank r on r.id = u.\"summonerId\" WHERE u.id <> $1 AND u.id not in(select id_match from duo.user_match um where um.id = $1) order by u.id, r.\"queueType\";", match.Id)
+	rows, err := s.Query("SELECT u.id, u.\"summonerId\", u.name, u.email, r.\"queueType\", r.tier, r.rank, r.\"leaguePoints\", r.wins, r.losses FROM duo.\"user\" u join duo.rank r on r.id = u.\"summonerId\" WHERE u.id <> $1 AND u.id not in(select id_match from duo.user_match um where um.id = $1) order by u.id, r.\"queueType\" desc;", match.Id)
 	if err != nil {
 		return nil, err
 	}
@@ -98,7 +98,7 @@ func (match *Match) FindNew() (Users, error) {
 func (match *Match) FindAll() (Users, error) {
 	s := GetDB()
 	var array Users
-	rows, err := s.Query("SELECT u.id, u.\"summonerId\", u.name, u.email, r.\"queueType\", r.tier, r.rank, r.\"leaguePoints\", r.wins, r.losses FROM duo.\"user\" u join duo.rank r on r.id = u.\"summonerId\" WHERE u.id <> $1 AND u.id not in(select id_match from duo.user_match um where um.id = $1) order by u.id, r.\"queueType\";", match.Id)
+	rows, err := s.Query("SELECT u.id, u.\"summonerId\", u.name, u.email, r.\"queueType\", r.tier, r.rank, r.\"leaguePoints\", r.wins, r.losses FROM duo.\"user\" u join duo.rank r on r.id = u.\"summonerId\" WHERE u.id <> $1 AND u.id not in(select id_match from duo.user_match um where um.id = $1) order by u.id, r.\"queueType\" desc;", match.Id)
 	if err != nil {
 		return nil, err
 	}
